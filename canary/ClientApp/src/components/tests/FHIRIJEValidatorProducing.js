25828e39940f8bcabe9c3a915a5f9a777796fb15
Sarah MacAdam <smacadam@mitre.org>
2021-06-01 13:38:01 -0400

    Compare fhir with IJE

--- canary/ClientApp/src/components/tests/FHIRIJEValidatorProducing.js
+++ canary/ClientApp/src/components/tests/FHIRIJEValidatorProducing.js
@@ -2,71 +2,52 @@ import axios from 'axios';
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Breadcrumb, Button, Container, Divider, Grid, Header, Icon, Table } from 'semantic-ui-react';
+import { Breadcrumb, Button, Container, Divider, Grid, Header, Icon, Table, Form, Statistic } from 'semantic-ui-react';
 import { connectionErrorToast } from '../../error';
+import { toast } from 'react-semantic-toasts';
 import { Getter } from '../misc/Getter';
+import { FHIRInfo } from '../misc/info/FHIRInfo';
 import { Record } from '../misc/Record';
+import report from '../report';
 
 export class FHIRIJEValidatorProducing extends Component {
   displayName = FHIRIJEValidatorProducing.name;
 
   constructor(props) {
     super(props);
-    this.state = { ...this.props, fhirrecord: null, ijerecord: null, fhirInfo: null, results: null, issues: [], column: 'number', direction: 'ascending' };
-    this.updateTest = this.updateTest.bind(this);
+    this.state = { ...this.props, test: null, fhirrecord: null, ijerecord: null, fhirInfo: null, fhirIssues:null, results: null, ijeIssues: [], column: 'number', direction: 'ascending' };
     this.runTest = this.runTest.bind(this);
     this.updateRecord = this.updateRecord.bind(this);
     this.updateFHIRRecord = this.updateFHIRRecord.bind(this);
   }
 
-  componentDidMount() {
+  createTest() {
+    console.log("Fetch, set test");
     var self = this;
-    if (!!this.props.match.params.id) {
+    this.setState({ loading: true }, () => {
       axios
-        .get(window.API_URL + '/tests/' + this.props.match.params.id)
+        .post(window.API_URL + '/tests/validator', this.setEmptyToNull(this.state.ijerecord.fhirInfo))
         .then(function(response) {
           var test = response.data;
           test.results = JSON.parse(test.results);
-          self.setState({ test: test, fhirInfo: JSON.parse(response.data.referenceRecord.fhirInfo), loading: false });
+          console.log("The test id: " + test.testId);
+          self.setState({ test: test, fhirInfo: JSON.parse(response.data.referenceRecord.fhirInfo), loading: false }, () => {
+            toast({
+              type: 'success',
+              icon: 'check',
+              title: 'Success',
+              description: 'The IJE file was successfully uploaded',
+              time: 5000,
+            });
+          });
         })
         .catch(function(error) {
           self.setState({ loading: false }, () => {
             connectionErrorToast(error);
           });
         });
-    } else {
-      axios
-        .get(window.API_URL + '/tests/new')
-        .then(function(response) {
-          self.setState({ test: response.data, fhirInfo: JSON.parse(response.data.referenceRecord.fhirInfo), loading: false });
-        })
-        .catch(function(error) {
-          self.setState({ loading: false }, () => {
-            connectionErrorToast(error);
-          });
-        });
-    }
+    });
   }
-  
-  // fetchTest() {
-  //   var self = this;
-  //   if (!!this.props.match.params.id && !!this.ijerecord) {
-  //     this.setState({ loading: true }, () => {
-  //       self.setState({ fhirInfo: JSON.parse(this.ijerecord.fhirInfo), loading: false });
-  //     }).catch(function(error) {
-  //       self.setState({ loading: false }, () => {
-  //         connectionErrorToast(error);
-  //       });
-  //     });
-  //   }
-  // }
-
-  // updateFHIRInfo(_, data) {
-  //   if (!!data && !!data.value) {
-  //     console.log("updated fhir")
-  //     this.setState({ certificateNumber: data.value }, () => this.fetchTest());
-  //   }
-  // }
 
   setEmptyToNull(obj) {
     const o = JSON.parse(JSON.stringify(obj));
@@ -81,57 +62,33 @@ export class FHIRIJEValidatorProducing extends Component {
 
   updateRecord(record, issues) {
     if (record && record.ijeInfo) {
-      // store ije record
-      this.setState({ ijerecord: record, issues: [], results: record.ijeInfo }, () => {
-        document.getElementById('scroll-to-ije').scrollIntoView({ behavior: 'smooth', block: 'start' });
-      });
+      this.setState({ ijerecord: record, ijeIssues: [], results: record.ijeInfo }, () => this.createTest());
     } else if (issues && issues.length > 0) {
-      this.setState({ ijerecord: null, issues: issues, results: null });
+      this.setState({ ijerecord: null, ijeIssues: issues, results: null });
     }
   }
 
   updateFHIRRecord(record, issues) {
     if (record && record.fhirInfo) {
       // store fhir record
-      this.setState({ fhirrecord: record, issues: [], results: record.fhirInfo });
+      this.setState({ fhirrecord: record, fhirIssues: [] });
     } else if (issues && issues.length > 0) {
-      this.setState({ fhirrecord: null, issues: issues, results: null });
+      this.setState({ fhirrecord: null, fhirIssues: issues });
     }
-  }
-
-
-  handleSort = clickedColumn => () => {
-    const column = this.state.column;
-    const results = this.state.results;
-    const direction = this.state.direction;
-    if (column !== clickedColumn) {
-      this.setState({
-        column: clickedColumn,
-        results: _.sortBy(results, [clickedColumn]),
-        direction: 'ascending',
-      });
-      return;
-    }
-    this.setState({
-      results: results.reverse(),
-      direction: direction === 'ascending' ? 'descending' : 'ascending',
-    });
-  };
-
-  updateTest(test) {
-    this.setState({ test: test });
   }
 
   runTest() {
     var self = this;
+
     this.setState({ running: true }, () => {
       axios
-        .post(window.API_URL + '/tests/Produce/run/' + this.state.test.testId, this.setEmptyToNull(this.state.ijerecord.fhirInfo))
+        .post(window.API_URL + '/tests/Produce/run/' + this.state.test.testId, this.setEmptyToNull(this.state.fhirrecord.fhirInfo))
         .then(function(response) {
           var test = response.data;
           test.results = JSON.parse(test.results);
+          console.log(test.results);
           self.setState({ test: test, running: false }, () => {
-            document.getElementById('scroll-to-fhir').scrollIntoView({ behavior: 'smooth', block: 'start' });
+            document.getElementById('scroll-to').scrollIntoView({ behavior: 'smooth', block: 'start' });
           });
         })
         .catch(function(error) {
@@ -146,7 +103,7 @@ export class FHIRIJEValidatorProducing extends Component {
     return (
       <React.Fragment>
         <Grid>
-          <Grid.Row>
+          <Grid.Row id="scroll-to">
             <Breadcrumb>
               <Breadcrumb.Section as={Link} to="/">
                 Dashboard
@@ -155,121 +112,110 @@ export class FHIRIJEValidatorProducing extends Component {
               <Breadcrumb.Section>Validate FHIR Death Records (Producing)</Breadcrumb.Section>
             </Breadcrumb>
           </Grid.Row>
-          <Grid.Row>
-            <Container fluid>
-              <Divider horizontal />
-              <Header as="h2" dividing id="step-1">
-                <Icon name="upload" />
-                <Header.Content>
-                  Step 1: Upload IJE Record
-                  <Header.Subheader>
-                    Upload the IJE record you will use to validate the FHIR record. The below prompt allows you to paste the record, upload it as a file, or POST to Canary.
-                  </Header.Subheader>
-                </Header.Content>
-              </Header>
-              <div className="p-b-15" />
-              <Getter updateRecord={this.updateRecord} ijeOnly noFormat />
-            </Container>
-          </Grid.Row>
-          <div className="p-b-15" />
-          {!!this.state.issues && this.state.issues.length > 0 && (
-            <Grid.Row>
-              <Record record={null} issues={this.state.issues} showIssues />
-            </Grid.Row>
-          )}
-          {this.state.issues.length === 0 && !!this.state.ijerecord && !!this.state.results && (
-            <Grid.Row>
+          {!!this.state.test && this.state.test.completedBool && (
+            <Grid.Row className="loader-height">
               <Container>
-                <Divider horizontal>
-                  <Header as="h2">
-                    <Icon name="search" />
-                    IJE Mortality Record Details
-                  </Header>
-                </Divider>
+                <div className="p-b-10" />
+                <Statistic.Group widths="three">
+                  <Statistic size="large">
+                    <Statistic.Value>{this.state.test.total}</Statistic.Value>
+                    <Statistic.Label>Properties Checked</Statistic.Label>
+                  </Statistic>
+                  <Statistic size="large" color="green">
+                    <Statistic.Value>
+                      <Icon name="check circle" />
+                      {this.state.test.correct}
+                    </Statistic.Value>
+                    <Statistic.Label>Correct</Statistic.Label>
+                  </Statistic>
+                  <Statistic size="large" color="red">
+                    <Statistic.Value>
+                      <Icon name="times circle" />
+                      {this.state.test.incorrect}
+                    </Statistic.Value>
+                    <Statistic.Label>Incorrect</Statistic.Label>
+                  </Statistic>
+                </Statistic.Group>
+                <Grid centered columns={1} className="p-t-30 p-b-15">
+                  <Button icon labelPosition='left' primary onClick={() => this.downloadAsFile(report(this.state.test, this.connectathonRecordName(this.props.match.params.id)))}><Icon name='download' />Generate Downloadable Report</Button>
+                </Grid>
+                <div className="p-b-20" />
+                <Form size="large">
+                  <FHIRInfo fhirInfo={this.state.test.results} editable={false} testMode={true} updateFhirInfo={null} />
+                </Form>
               </Container>
-              <Table celled striped sortable id="scroll-to-ije">
-                <Table.Header>
-                  <Table.Row>
-                    <Table.HeaderCell sorted={this.state.column === 'name' ? this.state.direction : null} onClick={this.handleSort('name')}>
-                      Name
-                    </Table.HeaderCell>
-                    <Table.HeaderCell sorted={this.state.column === 'number' ? this.state.direction : null} onClick={this.handleSort('number')}>
-                      Number
-                    </Table.HeaderCell>
-                    <Table.HeaderCell sorted={this.state.column === 'location' ? this.state.direction : null} onClick={this.handleSort('location')}>
-                      Location
-                    </Table.HeaderCell>
-                    <Table.HeaderCell sorted={this.state.column === 'length' ? this.state.direction : null} onClick={this.handleSort('length')}>
-                      Length
-                    </Table.HeaderCell>
-                    <Table.HeaderCell sorted={this.state.column === 'contents' ? this.state.direction : null} onClick={this.handleSort('contents')}>
-                      Contents
-                    </Table.HeaderCell>
-                    <Table.HeaderCell sorted={this.state.column === 'value' ? this.state.direction : null} onClick={this.handleSort('value')}>
-                      Value
-                    </Table.HeaderCell>
-                  </Table.Row>
-                </Table.Header>
-                <Table.Body>
-                  {!!this.state.results &&
-                    this.state.results.map(field => (
-                      <Table.Row key={'row' + field.name}>
-                        <Table.Cell>{field.name}</Table.Cell>
-                        <Table.Cell>{field.number}</Table.Cell>
-                        <Table.Cell>{field.location}</Table.Cell>
-                        <Table.Cell>{field.length}</Table.Cell>
-                        <Table.Cell>{field.contents}</Table.Cell>
-                        <Table.Cell>{field.value}</Table.Cell>
-                      </Table.Row>
-                    ))}
-                </Table.Body>
-              </Table>
             </Grid.Row>
           )}
-          <Grid.Row>
-            <Container fluid>
-              <Divider horizontal />
-              <Header as="h2" dividing id="step-2">
-                <Icon name="upload" />
-                <Header.Content>
-                  Step 2: Upload FHIR Record
-                  <Header.Subheader>Upload the FHIR record to validate. The below prompt allows you to paste the record, upload it as a file, or POST to Canary.</Header.Subheader>
-                </Header.Content>
-              </Header>
+          {!(!!this.state.test && this.state.test.completedBool) && (
+            <React.Fragment>
+              <Grid.Row>
+                <Container fluid>
+                  <Divider horizontal />
+                  <Header as="h2" dividing id="step-1">
+                    <Icon name="upload" />
+                    <Header.Content>
+                      Step 1: Upload IJE Record
+                      <Header.Subheader>
+                        Upload the IJE record you will use to validate the FHIR record. The below prompt allows you to paste the record, upload it as a file, or POST to Canary.
+                      </Header.Subheader>
+                    </Header.Content>
+                  </Header>
+                  <div className="p-b-15" />
+                  <Getter updateRecord={this.updateRecord} ijeOnly />
+                </Container>
+              </Grid.Row>
               <div className="p-b-15" />
-              {!!this.state.issues && this.state.issues.length > 0 && (
-                <Grid.Row id="scroll-to-fhir">
-                  <Record issues={this.state.issues} showIssues />
+              {!!this.state.ijeIssues && this.state.ijeIssues.length > 0 && (
+                <Grid.Row>
+                  <Record record={null} issues={this.state.ijeIssues} showIssues />
                 </Grid.Row>
               )}
-              <Getter updateRecord={this.updateFHIRRecord} allowIje={false} />
-            </Container>
-          </Grid.Row>
-          <Grid.Row>
-            <Container fluid>
-              <Divider horizontal />
-              <Header as="h2" dividing className="p-b-5" id="step-3">
-                <Icon name="check circle" />
-                <Header.Content>
-                  Step 3: Calculate Results
-                  <Header.Subheader>
-                    When you have imported both records, click the button below and Canary will calculate the results of the test.
-                  </Header.Subheader>
-                </Header.Content>
-              </Header>
-              <div className="p-b-10" />
-              <Button
-                fluid
-                size="huge"
-                primary
-                onClick={this.runTest}
-                loading={this.state.running}
-                disabled={!!!(this.state.fhirrecord && this.state.fhirrecord.xml)}
-              >
-                Calculate
-              </Button>
-            </Container>
-          </Grid.Row>
+              <Grid.Row>
+                <Container fluid>
+                  <Divider horizontal />
+                  <Header as="h2" dividing id="step-2">
+                    <Icon name="upload" />
+                    <Header.Content>
+                      Step 2: Upload FHIR Record
+                      <Header.Subheader>Upload the FHIR record to validate. The below prompt allows you to paste the record, upload it as a file, or POST to Canary.</Header.Subheader>
+                    </Header.Content>
+                  </Header>
+                  <div className="p-b-15" />
+                  {!!this.state.fhirIssues && this.state.fhirIssues.length > 0 && (
+                    <Grid.Row id="scroll-to-fhir">
+                      <Record issues={this.state.fhirIssues} showIssues />
+                    </Grid.Row>
+                  )}
+                  <Getter updateRecord={this.updateFHIRRecord} allowIje={false} />
+                </Container>
+              </Grid.Row>
+              <Grid.Row>
+                <Container fluid>
+                  <Divider horizontal />
+                  <Header as="h2" dividing className="p-b-5" id="step-3">
+                    <Icon name="check circle" />
+                    <Header.Content>
+                      Step 3: Calculate Results
+                      <Header.Subheader>
+                        When you have imported both records, click the button below and Canary will calculate the results of the test.
+                      </Header.Subheader>
+                    </Header.Content>
+                  </Header>
+                  <div className="p-b-10" />
+                  <Button
+                    fluid
+                    size="huge"
+                    primary
+                    onClick={this.runTest}
+                    loading={this.state.running}
+                    disabled={!!!(this.state.fhirrecord && this.state.fhirrecord.xml)}
+                  >
+                    Calculate
+                  </Button>
+                </Container>
+              </Grid.Row>
+            </React.Fragment>
+          )}
         </Grid>
       </React.Fragment>
     );
