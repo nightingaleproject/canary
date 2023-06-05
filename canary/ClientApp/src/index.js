111f9be700e676debd46528be74982ddd1747c1b
Thomas Nguyen <118298834+consultantusa@users.noreply.github.com>
2023-06-05 17:31:27 -0500

    Update index.js

--- canary/ClientApp/src/index.js
+++ canary/ClientApp/src/index.js
@@ -11,7 +11,7 @@ const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 //window.API_URL = 'http://localhost:5000';
 window.API_URL = '';
 window.VERSION = '4.0.2';
-window.VERSION_DATE = 'May 16th, 2023';
+window.VERSION_DATE = 'June 5th, 2023';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
