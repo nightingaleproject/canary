2eb8e3c451cd6bd715557c75fa86408c3bf26b0f
Le Yang <le.yang@gmail.com>
2023-06-06 19:46:45 +0000

    update test cases

--- canary/ClientApp/src/components/misc/__tests__/Record.test.js
+++ canary/ClientApp/src/components/misc/__tests__/Record.test.js
@@ -2,6 +2,12 @@ import renderer from 'react-test-renderer';
 import {Record} from '../Record';
 
 it('renders correctly', () => {
-  const tree = renderer.create(<Record record="{" />).toJSON();
+  const record = {
+    json: '{"a":2}',
+    xml: '<a>b</a>',
+    hideIje: true
+  }
+
+  const tree = renderer.create(<Record record={record} />).toJSON();
   expect(tree).toMatchSnapshot();
 });
