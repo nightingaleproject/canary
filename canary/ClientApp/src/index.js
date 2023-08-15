0f9e739c09374c506a11caccaf00d1ce372ffc15
Yang, Le <le.yang@gmail.com>
2023-08-15 10:16:42 -0400

    docs: update version notes

--- canary/ClientApp/src/index.js
+++ canary/ClientApp/src/index.js
@@ -10,10 +10,10 @@ const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 
 //window.API_URL = 'http://localhost:5000';
 window.API_URL = '';
-window.VERSION = '4.0.5';
-window.VERSION_DATE = 'August 9, 2023';
-window.VRDR_VERSION = '4.1.3';
-window.VRDR_VERSION_DATE = 'August 5, 2023'; 
+window.VERSION = '4.0.7';
+window.VERSION_DATE = 'August 15, 2023';
+window.VRDR_VERSION = '4.1.4';
+window.VRDR_VERSION_DATE = 'August 15, 2023'; 
 
 const container = document.getElementById('root');
 const root = createRoot(container);
