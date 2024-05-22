17982e620b20cde01d4612b9a40b7252fd1c1587
Le Yang <le.yang@gmail.com>
2024-05-22 12:29:49 +0000

    chore: bump VRDR .NET to 4.1.13

--- canary/ClientApp/src/index.js
+++ canary/ClientApp/src/index.js
@@ -10,10 +10,10 @@ const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 
 //window.API_URL = 'http://localhost:5000';
 window.API_URL = '';
-window.VERSION = '4.0.11';
-window.VERSION_DATE = 'May 9, 2024';
-window.VRDR_VERSION = '4.1.11';
-window.VRDR_VERSION_DATE = 'May 3, 2024';
+window.VERSION = '4.1.0';
+window.VERSION_DATE = 'May 22, 2024';
+window.VRDR_VERSION = '4.1.13';
+window.VRDR_VERSION_DATE = 'May 22, 2024';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
